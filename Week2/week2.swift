7242d9c6d9e6b676a98f6b879a03e4e2ff6f95bd
And G <123909107+Angela-is-here@users.noreply.github.com>
2023-02-07 15:35:31 -0500

    Update week2.swift

--- Week2/week2.swift
+++ Week2/week2.swift
@@ -1,6 +1,5 @@
 import UIKit
 
-// Read in an image from a url string
 func imageFor(_ str: String) -> UIImage {
     let url = URL(string: str)
     let imgData = try? Data(contentsOf: url!)
@@ -9,15 +8,15 @@ func imageFor(_ str: String) -> UIImage {
 }
 
 //image
-let u1 = "https://i.ibb.co/d0K6bbb/tumblr-344bca3e77091fdec95c69cc6d8a28c4-d757c70a-1280.png"
-let ui1 = imageFor(u1)
+let img1 = "https://i.ibb.co/d0K6bbb/tumblr-344bca3e77091fdec95c69cc6d8a28c4-d757c70a-1280.png"
+let img1 = imageFor(u1)
 
 
-let sz = CGSize(width: 1024, height: 1024)
-let renderer = UIGraphicsImageRenderer(size: sz)
+let size = CGSize(width: 1024, height: 1024)
+let renderer = UIGraphicsImageRenderer(size: size)
 
 let image = renderer.image { (context) in
-    ui1.draw(in: CGRect(x: 0, y: 0, width: 1024, height: 1024))
+    img1.draw(in: CGRect(x: 0, y: 0, width: 1024, height: 1024))
 
 }
 
